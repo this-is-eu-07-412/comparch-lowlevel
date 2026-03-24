e630a1abd7496424ee5e05d9abe3641f70cf64f9
szalandajulian-eng <szalanda.julian@icloud.com>
2026-03-24 16:40:05 +0100

    funkcjonalny kod z opisem

--- task_1/e_prog2.asm
+++ task_1/e_prog2.asm
@@ -3,45 +3,44 @@
 ; Exercise       : Compilation, linking and debugging of assembly programs    ;
 ; Authors        : First Name Surname, group number                           ;
 ; Date           : DD.MM.YY                                                   ;
-; Notes          : Program which which finds the largest number in a table    ;
+; Notes          : Program which finds the largest number in a table          ;
 ;                                                                             ;
 ;=============================================================================;
 
                 .MODEL  SMALL
+                .STACK  100h            ; Fixed typo .STCK
 
                 .DATA
-TABLEN          DB     12
-Array           DB     01h, 02h, 00h, 10h, 12h, 33h
-                DB     15h, 09h, 11h, 08h, 0Ah, 00h
-Largest         DB     12
+TABLEN          EQU     12              ; Constant: number of elements in table
+Table           DB      01h, 02h, 00h, 10h, 12h, 33h
+                DB      15h, 09h, 11h, 08h, 0Ah, 00h
+Largest         DB      0               ; Variable to store the result
 
                 .CODE
-Start:			mov     ax, @DATA
+Start:
+                ; Initialize Data Segment
+                mov     ax, @DATA
                 mov     ds, ax
-				mov 	bx, OFFSET Array
-		
-;Loop:			mov     cl, TABLEN
-;
-;				cmp     
-				
-;				dec 	cl
 
-                mov     al, Largest
-                mov     ah, 4Ch
+                ; Prepare for the search loop
+                mov     cx, TABLEN      ; Load loop counter (12 elements)
+                mov     bx, OFFSET Table ; Point BX to the start of the table
+                mov     al, 0           ; AL will hold the current maximum value
+
+FindMax:
+                cmp     al, [bx]        ; Compare current max (AL) with table element
+                jae     NextElement     ; If AL >= [bx], skip updating max
+                mov     al, [bx]        ; If AL < [bx], update AL with new max
+
+NextElement:
+                inc     bx              ; Move to the next byte in the table
+                loop    FindMax         ; Decrement CX and jump to FindMax if CX > 0
+
+                ; Store final result in memory
+                mov     Largest, al     ; Move the largest found value to Largest variable
+
+                ; Terminate program
+                mov     ax, 4C00h
                 int     21h
                 
-                .STACK    100h
-                END  Start
-
-
-;                mov     bx, OFFSET TABLEN
-;                mov     al, [bx]
-;
-;                mov     ch, Table
-;Loop:
-;                cmp     al, [dx]
-;                jbe     Start
-;                muv     ah, [bx]
-;Jump:
-;                inc     bh
-;                jmp    Jump
+                END     Start           ; Point to the entry label
