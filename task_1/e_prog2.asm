e55ce5fd7e4fbbf01744f123cf5940c3a50e3ffb
this-is-eu-07-412 <kabanoskikabanosek@gmail.com>
2026-03-24 19:11:31 +0000

    Tweaks to program 2

--- task_1/e_prog2.asm
+++ task_1/e_prog2.asm
@@ -8,39 +8,32 @@
 ;=============================================================================;
 
                 .MODEL  SMALL
-                .STACK  100h            ; Fixed typo .STCK
-
+                
                 .DATA
-TABLEN          EQU     12              ; Constant: number of elements in table
-Table           DB      01h, 02h, 00h, 10h, 12h, 33h
+TABLENGTH       EQU     12              	; Constant: number of elements in table
+Table1          DB      01h, 02h, 00h, 10h, 12h, 33h
                 DB      15h, 09h, 11h, 08h, 0Ah, 00h
-Largest         DB      0               ; Variable to store the result
+Largest         DB      ?               	; Variable to store the result
 
                 .CODE
-Start:
-                ; Initialize Data Segment
-                mov     ax, @DATA
+Start:          mov     ax, @DATA			; Initialize Data Segment
                 mov     ds, ax
+                mov     cx, TABLENGTH    	; Load loop counter (12 elements)
+                mov     bx, OFFSET Table1	; Point BX to the start of the table
+                mov     al, [bx]           	; AL will hold the current maximum value, starting from first element in the table
+;				dec 	cx					; These two (dec and inc) just make sure we don't waste time on comparing the first element with itself
+;				inc 	bx					; But the program will work as intended without them (uncomment if needed)
+				
+FindMax:        cmp     al, [bx]        	; Compare current max (AL) with table element
+                jae     NextElement     	; If AL >= [bx], skip updating max
+                mov     al, [bx]       		; If AL < [bx], update AL with new max
 
-                ; Prepare for the search loop
-                mov     cx, TABLEN      ; Load loop counter (12 elements)
-                mov     bx, OFFSET Table ; Point BX to the start of the table
-                mov     al, 0           ; AL will hold the current maximum value
-
-FindMax:
-                cmp     al, [bx]        ; Compare current max (AL) with table element
-                jae     NextElement     ; If AL >= [bx], skip updating max
-                mov     al, [bx]        ; If AL < [bx], update AL with new max
-
-NextElement:
-                inc     bx              ; Move to the next byte in the table
-                loop    FindMax         ; Decrement CX and jump to FindMax if CX > 0
-
-                ; Store final result in memory
-                mov     Largest, al     ; Move the largest found value to Largest variable
-
-                ; Terminate program
-                mov     ax, 4C00h
-                int     21h
+NextElement:    inc     bx              	; Move to the next byte in the table
+                loop    FindMax         	; Decrement CX and jump to FindMax if CX > 0
+				
+                mov     Largest, al     	; Store final result in memory (not required)
+                mov     ah, 4Ch				; Terminate program returning the largest value as return code
+                int     21h					
                 
-                END     Start           ; Point to the entry label
+				.STACK  100h            
+                END     Start           	; Point to the entry label
